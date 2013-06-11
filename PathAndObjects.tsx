605987c1c6cba72364b75017aa7f6d426f82572b
Nilan <nilan.marktanner@student.kit.edu>
2013-06-11 22:50:37 +0200

    First commit.
    This is a project not only at a starting stage, but I started
    
    with using git only now. I know, I'm a bad person.
    
    I will add credits later (sounds, tilesets, etc).
    
    Next step will be to add a kezmenu :)

---
+++ PathAndObjects.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="PathAndObjects" tilewidth="32" tileheight="32">
+ <tileoffset x="1" y="1"/>
+ <image source="PathAndObjects.png" width="512" height="512"/>
+</tileset>
